dac9da3c0f4010299ad710c15337482d916c6aba
Rahul Kumar <152783083+fired-rahul-udp-80@users.noreply.github.com>
2025-01-23 17:37:05 +0530

    Update Notes.jsx

--- src/components/home/Notes.jsx
+++ src/components/home/Notes.jsx
@@ -22,7 +22,7 @@ const Notes = () => {
 
   return (
     <div>
-        <div className="mb-10">
+        <div className="mb-10 mx-10">
             <ul className="flex font-semibold md:text-xl shadow-xl overflow-x-scroll no-scrollbar" >
                     <li onClick={() =>setSemester1Details(true) & setSemester2Details(false) & setSemester3Details(false) & setSemester4Details(false) & setFinalYearProject(false)}
                      className={`px-6 md:px-10 py-5 bg-bgColor text-secondaryColor font-titleFont hover:bg-boxBg hover:opacity-90 hover:text-opacity-70 cursor-pointer text-center w-full
