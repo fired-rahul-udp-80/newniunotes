40ad84fea8216d4a04261a808f4d94ac301543fa
Rahul Kumar <152783083+fired-rahul-udp-80@users.noreply.github.com>
2025-01-31 23:38:39 +0530

    Update AnchorTag.jsx

--- src/components/template/AnchorTag.jsx
+++ src/components/template/AnchorTag.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AnchorTag = ({text, linkto, active}) => {
   return (
     <div className="w-full">
-        <a href={linkto} target="_blank" className={`flex justify-center px-8 py-4 text-white font-semibold font-titleFont rounded hover:opacity-90 text-md ${active ? "bg-bgColor border-[1px] border-black":"bg-gray-900 border-[1px] border-bgColor" } `}>{text}</a>
+        <a href={linkto} target="_blank" className={`flex justify-center px-8 py-4 text-white font-semibold font-titleFont rounded hover:opacity-90 text-md ${active ? "bg-bgColor border-b-[7px] border-r-[7px] border-red-950":"bg-gray-900 border-b-[7px] border-r-[7px] border-gray-900" } `}>{text}</a>
     </div> 
   )
 }
